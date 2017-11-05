bdbd9e683b55ad1c80af4e9f48b063fc58357837
jakalaka <jakalaka@aliyun.com>
2017-11-05 13:43:02 +0800

    mixClient

--- createJAR.bat
+++ createJAR.bat
@@ -1,3 +1,2 @@
-cd Desktop
 wsimport  -keep http://localhost:9000/CommonTran?wsdl
 wsimport -clientjar Com.jar -keep http://localhost:9000/CommonTran?wsdl
--- src/main/java/XQBHClient/Utils/PropertiesHandler/PropertiesReader.java
+++ src/main/java/XQBHClient/Utils/PropertiesHandler/PropertiesReader.java
@@ -2,36 +2,29 @@ package XQBHClient.Utils.PropertiesHandler;
 
 import XQBHClient.Utils.log.Logger;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
+import java.net.URI;
 import java.util.Properties;
 
 /**
  * Created by Administrator on 2017/7/1 0001.
  */
 public class PropertiesReader {
-    public static String readKeyFromXML(File file,String Key){
+    public static String readKeyFromXML(String path, String Key){
         Properties propertie;
-        FileInputStream inputFile;
-        if (!file.exists())
-        {
-            Logger.log("LOG_ERR","file not found");
-            return  null;
-        }
+        InputStream inputStream=Class.class.getResourceAsStream(path);
+
 
         propertie = new Properties();
         try{
-            inputFile = new FileInputStream(file);
-            propertie.load(inputFile);
+            propertie.load(inputStream);
 //            propertie.loadFromXML(inputFile);//读取XML文件
-            inputFile.close();
+            inputStream.close();
         } catch (FileNotFoundException ex){
-            Logger.log("LOG_ERR","文件无法找到");
+            Logger.log("LOG_ERR","文件"+path+"无法找到");
             ex.printStackTrace();
         } catch (IOException ex) {
-            Logger.log("LOG_ERR","读取文件失败");
+            Logger.log("LOG_ERR","读取文件"+path+"失败");
             ex.printStackTrace();
         }
         return propertie.getProperty(Key);
